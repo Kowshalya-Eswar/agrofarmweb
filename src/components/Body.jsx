9546c514a142364fa14a1c784a3ff3480905a987
kowshalya <kowshalya@tigeen.ch>
2025-06-19 09:41:26 +0530

    profile changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,32 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./NavBar"
 import Footer from "./Footer";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 const Body = () =>{
+    const { currentUser } = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    async function fetchUser() {
+        if (currentUser !== null) return;
+        try {
+            const res = await axios.get(BASE_URL + 'profile',{
+                withCredentials: true
+            })
+            dispatch(addUser(res.data.data));
+        } catch (error) {
+            if (error.status == 401) {
+                navigate("/login");
+            }
+        }
+    };
+    useEffect(()=>{
+        fetchUser()
+    },[]);
     return (
     <div>
         <Navbar/>
