8d7f359fbe3c4e3cb1d679b8136a7925898dedf7
kowshalya <kowshalya@tigeen.ch>
2025-06-28 13:38:08 +0530

    image changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 //export const BASE_URL = "http://localhost:7777/api";
 export const BASE_URL = "/api";
 //export const DOMAIN_URL = "http://localhost:7777";
-export const DOMAIN_URL = "http://65.0.19.191";
+export const DOMAIN_URL = "";
